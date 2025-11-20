a348959d2fa39a8a6d41e44156d1b46ba9a07d65
Nocturna2428 <162833266+Nocturna2428@users.noreply.github.com>
2025-11-19 21:34:18 -0700

    Add binary state machine module
    Implement a binary state machine with D flip-flops.

--- binary.v
+++ binary.v
@@ -1,1 +1,26 @@
-// Implement binary state machine
+module binary(
+    input w,
+    input clk,
+    output z
+);
+
+    wire [1:0] State;
+    wire [1:0] Next;
+// this is the action and state for some buttons
+    dff zero(
+        .D(Next[0]),
+        .clk(clk),
+        .Q(State[0])
+    );
+
+    dff one(
+        .D(Next[1]),
+        .clk(clk),
+        .Q(State[1])
+    );
+
+    assign z = State[1] & ~State[0];
+    assign Next[0] = w & ~State[1] & ~State[0];
+    assign Next[1] = w & (State[1] | State[0]);
+
+endmodule
