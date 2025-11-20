72eae38a70bfa08acfd933a709aaaf3b35f8c047
Nocturna2428 <162833266+Nocturna2428@users.noreply.github.com>
2025-11-19 22:46:19 -0700

    Update onehot.v

--- onehot.v
+++ onehot.v
@@ -1,38 +1,62 @@
 
 module one_hot(
-    input w,
-    input reset,
-    input clk,
+    input w, reset, clk,
     output z,
-    output [4:0] state_out //this is due to the flip-flop table
-);
+    output As, Bs, Cs, Ds, Es // they are A-E states
    //state flip-flops from the table 
-    reg A, B, C, D, E;
- // Output: z = 1 only in state C or E
-    assign z = C | E;
-
-    // This is here for debugging
-    assign state_out = {E, D, C, B, A};
+   
+);
+ wire Anext, Bnext, Cnext, Dnext, Enext; 
+  
     
     
-    wire Anext = (A & w == 0) ? 0 : 0; 
-    wire Bnext = (A & ~w) | (D & ~w) | (E & ~w);
-    wire Cnext = (B & ~w) | (C & ~w);
-    wire Dnext = (A & w) | (B & w) | (C & w);
-    wire Enext = (D & w) | (E & w);
-
-    // Sequential update
-    always @(posedge clk or posedge reset) begin
-        if (reset) begin
-            A <= 1; B <= 0; C <= 0; D <= 0; E <= 0;   // Initial state A
-        end
-        else begin
-            A <= 0;             // A is never a next state in this machine
-            B <= Bnext;
-            C <= Cnext;
-            D <= Dnext;
-            E <= Enext;
-        end
-    end
+    dff Adff(
+        .Default(1'b1),
+        .D(Anext),
+        .clk(clk),
+        .Q(Astate),
+        .reset(reset)
+        );
+        
+    dff Bdff(
+        .Default(1'b0),
+        .D(Bnext),
+        .clk(clk),
+        .Q(Bstate),
+        .reset(reset)
+         );
+         
+    dff Cdff(
+        .Default(1'b0),
+        .D(Cnext),
+        .clk(clk),
+        .Q(Cstate),
+        .reset(reset)
+        );
+        
+    dff Ddff(
+        .Default(1'b0),
+        .D(Dnext),
+        .clk(clk),
+        .Q(Dstate),
+        .reset(reset)
+         );
+     dff Edff(
+        .Default(1'b0),
+        .D(Enext),
+        .clk(clk),
+        .Q(Estate),
+        .reset(reset)
+         );
+           
+         
+    assign z = Cstate | Dstate;
+    
+    assign Anext = 0;
+    assign Bnext = (~w & Astate) | (~w & Dstate)| (~w & Estate);
+    assign Cnext = (~w & Bstate) | (~w & Cstate);
+    assign Dnext = (w & Astate) |(w & Bstate) |(w & Cstate);
+    assign Enext = (w & Dstate) | (w & Estate);
+    
 
 endmodule
