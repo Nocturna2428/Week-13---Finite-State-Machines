9ace6325ff93e96a33ff5160a5ef6b633d721254
Nocturna2428 <162833266+Nocturna2428@users.noreply.github.com>
2025-11-20 14:01:50 -0700

    Update top.v

--- top.v
+++ top.v
@@ -4,7 +4,27 @@ module top(
     input btnC, // clk
     input btnU // reset
 );
-
     // Hook up binary and one-hot state machines
-
+    oh hot(
+        .w(sw),
+        .clk(btnC),
+        .z(led[0]),
+        .Astate(led[2]),
+        .Bstate(led[3]),
+        .Cstate(led[4]),
+        .Dstate(led[5]),
+        .Estate(led[6]),
+        .reset(btnU)
+        );
+        
+    binary binary(
+        .w(sw),
+        .clk(btnC),
+        .z(led[1]),
+        .State0(led[7]),
+        .State1(led[8]),
+        .State2(led[9]),
+        .reset(btnU)
+        );
+        
 endmodule
