e9e92b99168645590f5fdd8bf8fae95da859fcca
Nocturna2428 <162833266+Nocturna2428@users.noreply.github.com>
2025-11-19 21:53:10 -0700

    Update onehot.v

--- onehot.v
+++ onehot.v
@@ -1,9 +1,19 @@
 
 module one_hot(
     input w,
+    input reset,
     input clk,
-    output z
+    output z,
+    output [4:0] state_out //this is due to the flip-flop table
 );
+   //state flip-flops from the table 
+    reg A, B, C, D, E;
+ // Output: z = 1 only in state C or E
+    assign z = C | E;
+
+    // This is here for debugging
+    assign state_out = {E, D, C, B, A};
+    
     wire Anext, Bnext, Cnext;
     wire Astate, Bstate, Cstate;
 
