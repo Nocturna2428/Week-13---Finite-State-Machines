e4c07c8f2a1fd89c804a814fb66fd0f1b8d0703a
Nocturna2428 <162833266+Nocturna2428@users.noreply.github.com>
2025-11-19 21:39:51 -0700

    Update onehot.v

--- onehot.v
+++ onehot.v
@@ -1,1 +1,36 @@
-// Implement one-hot state machine
+
+module one_hot(
+    input w,
+    input clk,
+    output z
+);
+    wire Anext, Bnext, Cnext;
+    wire Astate, Bstate, Cstate;
+
+    dff Adff(
+        .Default(1'b1),
+        .D(Anext),
+        .clk(clk),
+        .Q(Astate)
+    );
+
+    dff Bdff(
+        .Default(1'b0),
+        .D(Bnext),
+        .clk(clk),
+        .Q(Bstate)
+    );
+
+    dff Cdff(
+        .Default(1'b0),
+        .D(Cnext),
+        .clk(clk),
+        .Q(Cstate)
+    );
+
+    assign z = Cstate;
+
+    assign Anext = ~w;
+    assign Bnext = w & Astate;
+    assign Cnext = (w & Bstate) | (w & Cstate);
+endmodule
